7c3d65f9f11503fbe9fe3153e17d45580bb0ed58
CJBartel <bartel.chrisj@gmail.com>
2022-10-23 08:53:03 -0500

    making yaml available

--- setup.py
+++ setup.py
@@ -1,28 +1,92 @@
-from setuptools import setup, find_packages
+import io
 import os
+import sys
+from shutil import rmtree
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
+from setuptools import find_packages, setup, Command
 
-if __name__ == "__main__":
-    setup(
-        name='pydmc',
-        version='0.0.1',
-        description='facilitating efficient computational materials research',
-        long_description=open(os.path.join(module_dir, 'README.md')).read(),
-        url='https://github.umn.edu/bartel-group/pydmc',
-        author=['Bartel Research Group'],
-        author_email=['cbartel@umn.edu'],
-        license='MIT',
-        packages=find_packages(),
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=[],
-        extras_require={},
-        classifiers=[],
-        test_suite='',
-        tests_require=[],
-        scripts=[]
-    )
-    
-#        package_data={'modules' : ['module_data/*.json',
-#                                   'module_data/*.p']}
+NAME = 'pydmc'
+DESCRIPTION = 'Package to facilitate DFT calculations and analysis'
+URL = 'https://github.umn.edu/bartel-group/pydmc'
+EMAIL = 'cbartel@umn.edu'
+AUTHOR = 'Chris Bartel'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1.0'
+
+REQUIRED = ['numpy', 
+            'pymatgen']
+EXTRAS = {}
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
