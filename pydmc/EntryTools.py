bcf15216da8f9b3bca456b51dfb8decb2cb3cd52
CJBartel <bartel.chrisj@gmail.com>
2022-12-21 10:32:14 -0600

    EntryTools

--- pydmc/EntryTools.py
+++ pydmc/EntryTools.py
@@ -1,16 +1,15 @@
-from pymatgen.entries.computed_entries import ComputedEntry, ComputedStructureEntry, GibbsComputedStructureEntry
+from pymatgen.entries.computed_entries import ComputedEntry, ComputedStructureEntry
+from pymatgen.entries.compatibility import MaterialsProject2020Compatibility
 from pymatgen.core.structure import Structure
 from pydmc.CompTools import CompTools
 from pydmc.StrucTools import StrucTools
 from pydmc.VASPTools import VASPAnalysis
 import os
 
-class EntryTools(object):
+class MPCompatibilityTools(object):
     
     def __init__(self,
-                 data={},
-                 calc_dir=None,
-                 temperature=0):
+                 calc_dir):
         """
         Args:
             data (dict): dictionary of data to create entry
@@ -23,45 +22,47 @@ class EntryTools(object):
 
         """
         
-        self.data = data
         self.calc_dir = calc_dir
-        self.temperature = temperature
+        if not VASPAnalysis(calc_dir).is_converged:
+            raise ValueError('Calculation in %s is not converged' % calc_dir)
 
     @property
-    def entry(self):
-        data = self.data
+    def initial_entry(self):
         calc_dir = self.calc_dir
-        
-        if data and not calc_dir:
-            if 'formula' not in data or not data['formula']:
-                raise ValueError('No formula provided')
-            if 'E_per_at' not in data or not data['E_per_at']:
-                raise ValueError('No energy per atom provided')
-            return ComputedEntry(composition=data['formula'],
-                                 energy=data['E_per_at']*CompTools(data['formula']).n_atoms,
-                                 **data)
-            
-        if calc_dir:
-            structure = Structure.from_file(os.path.join(calc_dir, 'CONTCAR'))
-            formula = StrucTools(structure).compact_formula
-            n_atoms = CompTools(formula).n_atoms
-            E_per_at = VASPAnalysis(calc_dir).E_per_at
-            
-            T = self.temperature
-            
-            if T == 0:
-                return ComputedStructureEntry(structure=structure,
-                                            energy=E_per_at*n_atoms,
-                                            composition=formula,
-                                            **data)
-            else:
-                raise NotImplementedError('need formation energy at 0 K for finite T stuff in pmg, will work on later')
-            
+        structure = Structure.from_file(os.path.join(calc_dir, 'CONTCAR'))
+        formula = StrucTools(structure).compact_formula
+        n_atoms = CompTools(formula).n_atoms
+        E_per_at = VASPAnalysis(calc_dir).E_per_at
+        return ComputedStructureEntry(structure=structure,
+                                        energy=E_per_at*n_atoms,
+                                        composition=formula)
     @property
-    def mp_corrected_E_per_at(self):
-        entry = self.entry
-        correction_per_fu = entry.correction
-        original_E_per_fu = entry.energy
-        n_atoms = CompTools(entry.composition.pretty_formula).n_atoms
-        return (original_E_per_fu + correction_per_fu)/n_atoms
+    def compatibility(self):
+        return MaterialsProject2020Compatibility(compat_type='Advanced',
+                                                 correct_peroxide=True,
+                                                 check_potcar_hash=False,
+                                                 config_file=None)
         
+    @property
+    def adjustments(self):
+        return self.compatibility.get_adjustments(self.initial_entry)
+    
+    @property
+    def corrected_entry(self):
+        initial_entry = self.initial_entry
+        return ComputedEntry(composition=initial_entry.composition,
+                             energy=initial_entry.energy,
+                             energy_adjustments=self.adjustments)
+    
+    @property
+    def corrected_E_per_at(self):
+        entry = self.corrected_entry
+        uncorrected_E_per_at = entry.uncorrected_energy_per_atom
+        correction_per_at = entry.correction_per_atom
+        return uncorrected_E_per_at + correction_per_at
+    
+def main():
+    calc_dir = '~/projects/compare_to_mp/calcs/Fe1S2/mp-1522/mp/fm/ggau-static'
+    compat = MPCompatibilityTools(calc_dir=calc_dir)
+    
+    
