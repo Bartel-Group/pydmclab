4b6204715461ee194eff0ab0591e3f40c900e97f
cbartel <cbartel@umn.edu>
2023-06-05 10:38:34 -0500

    cjb ax_tdos edits

--- pydmclab/utils/plotting.py
+++ pydmclab/utils/plotting.py
@@ -12,6 +12,10 @@ from pydmclab.utils.handy import read_json, write_json
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+import numpy as np
+import random
+
+
 def get_colors(palette):
     """
 
@@ -36,6 +40,8 @@ def get_colors(palette):
             ax = plt.scatter(x, y, color=my_colors['blue'])
     """
     colors = get_color_palettes()[palette]
+    colors["black"] = (0, 0, 0)
+    colors["white"] = (1, 1, 1)
     return colors
 
 
@@ -73,18 +79,18 @@ def set_rc_params():
     return params
 
 
-# make slim tdos results 
+# make slim tdos results
 def make_smaller_results(xc=None, remake=False):
     # data directory
     DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
-    
+
     fjson = os.path.join(DATA_DIR, "tdos.json")
     if not remake and os.path.exists(fjson):
         return read_json(fjson)
 
     d = read_json(os.path.join(DATA_DIR, "results.json"))
     out = {}
-    
+
     # only keep the converged results and the xc you specified
     for k in d:
         if not d[k]["results"]["convergence"]:
@@ -96,8 +102,8 @@ def make_smaller_results(xc=None, remake=False):
     # keep whatever keys you need to plot tdos
     # Note: we need "structure" only if we want to normalize by formula unit
     keep_keys = ["results", "meta", "structure", "tdos"]
-    
-    for k in out:        
+
+    for k in out:
         for keep_key in keep_keys:
             out[k][keep_key] = d[k][keep_key].copy()
 
@@ -106,175 +112,258 @@ def make_smaller_results(xc=None, remake=False):
 
 
 # Please note that this function only deals with the case of total spin
-def plot_tdos(di, 
-        formula,
-        what_to_plot,
-        colors_and_labels,
-        title,
-        saveas,
-        normalization,
-        xlabel,
-        colors = get_colors('set2'),
-        xlim=(0, 10), ylim=(-2, 2), 
-        xticks=(True, [0, 5, 10]), yticks=(True, [-2, -1, 0, 1, 2]), 
-        ylabel=r'$E-E_F\/(eV)$',
-        legend=True,
-        smearing=0.2,
-        show=False,
-        ):
+def ax_tdos(
+    tdos,
+    population_sources="all",
+    colors={"total": "black"},
+    color_palette=get_colors("set2"),
+    params={"line_alpha": 0.9, "fill_alpha": 0.2, "lw": 1},
+    special_labels=None,
+    spins="summed",
+    normalization=None,
+    smearing=0.2,
+    Efermi=0.0,
+    xlim=(0, 10),
+    ylim=(-2, 2),
+    xticks=(True, [0, 5, 10]),
+    yticks=(True, [-2, -1, 0, 1, 2]),
+    xlabel="DOS",
+    ylabel=r"$E-E_F\/(eV)$",
+    legend=True,
+    title=None,
+    savename=False,
+    show=True,
+):
     """
     Args:
-        di (dict) - dictionary of results for every single calculation
-        formula - formula of compound
-        what_to_plot (dict) - {element or 'total' (str)}
-        colors_and_labels (dict) - {element-spin-orbital (str) : {'color' : color (str),
-                                                                  'label' : label (str)}}
-        title (str) - title of plot
-        saveas (str) - filename to save plot as
-        colors - please select the color you like
-        xlim (tuple) - (xmin (float), xmax (float))
-        ylim (tuple) - (ymin (float), ymax (float))
-        xticks (tuple) - (bool to show label or not, (xtick0, xtick1, ...))
-        yticks (tuple) - (bool to show label or not, (ytick0, ytick1, ...))
-        xlabel (str) - x-axis label
-        ylabel (str) - y-axis label
-        legend (bool) - include legend or not
-        smearing (float or False) - std. dev. for Gaussian smearing of DOS or False for no smearing         
-        normalization ('electron', 'formula') - divide populations by number of electrons or formula unit
-        show (bool) - if True, show figure; else just return ax
-                   
+        tdos (dict)
+            result of pydmclab.hpc.analyze.tdos()
+                list(d.keys()) = ['E', 'total', 'up', 'down'] + [list of elements (str)]
+                    d['E'] = 1d array of energies corresponding with DOS
+                    d[el] = 1d array of DOS for that element (sums all sites, orbitals, and spins)
+                    d['total'] = 1d array of DOS for structure (sums all elements, sites, orbitals, spins)
+                    d['up'] or d['down']:
+                        keys are ['total'] + [list of elements (str)]
+                        d['up']['total'] = 1d array of spin-up DOS for structure
+                        d['down'][el] = 1d array of spin-down DOS for that element
+                        etc
+                so if I wanted to plot the total DOS for my structure and separate spin up (+ DOS) and spin down (- DOS)
+                    energies = d['E']
+                    dos_up = d['up']['total']
+                    dos_down = d['down']['total']
+                    plt.plot(dos_up, energies)
+                    plt.plot(-1*dos_down, energies)
+
+        population_sources (str or list)
+            'all' : plot all elements and the total
+            ['total'] or 'total' : plots just the total
+            ['Al', 'N'] : plots just Al and N
+            ['Al'] or 'Al' : plots just Al
+
+        colors (dict)
+            {element or 'total' (str) : color (str)}
+
+        color_palette (dict)
+            {color (str) : rgb (tuple)}
+                usually loaded from pydmc.utils.plotting.get_colors()
+
+        params (dict)
+            {'fill_alpha' : transparency for occ populations,
+             'line_alpha' : transparency for DOS line,
+             'lw' : DOS linewidth}
+
+        special_labels (dict)
+            {element or 'total' (str) : label (str)}
+
+        spins (str)
+            'summed' : plot + and - DOS together as +
+            'separate' : plot + and - DOS separately as + and -
+                @TO-DO: implement separate spins
+
+        normalization (float)
+            divide DOS by this number
+                common normalizations:
+                    1 : no normalization (same as None)
+                    CompTools(formula).n_atoms (per formula unit)
+                    results['meta']['all_input_parameters']['NELECT'] (per electron)
+
+        smearing (float or False)
+            std. dev. for Gaussian smearing of DOS or False for no smearing
+
+        Efermi (float)
+            Fermi level (eV)
+
+        xlim (tuple)
+            (xmin (float), xmax (float))
+
+        ylim (tuple)
+            (ymin (float), ymax (float))
+
+        xticks (tuple)
+            (bool to show label or not, (xtick0, xtick1, ...))
+
+        xlabel (str)
+            x-axis label
+
+        ylabel (str)
+            y-axis label
+
+        legend (bool)
+            include legend
+
+        title (str)
+            title of plot
+
+        savename (str)
+            if False: don't save (just return ax)
+            if str, save fig object at this location
+
+        show (bool)
+            show plot
+
     Returns:
-        matplotlib axes object
+        matplotlib axis object
     """
     set_rc_params()
+    random.seed(42)
 
-    fig = plt.figure(figsize=(5,8))
+    if savename:
+        fig = plt.figure(figsize=(5, 8))
+
     ax = plt.subplot(111)
 
-    print("formula = ", formula)
-    
-    Efermi = 0.
+    Efermi = 0.0
     occupied_up_to = Efermi
-    print('Fermi level = ',occupied_up_to)
+    print("Fermi level = ", occupied_up_to)
 
-    population = {}
-    
-    #normalized dos w/ number of electrons or formula units
-    if normalization == 'electron':
-        normalization = di['meta']['all_input_settings']['NELECT']
-        print('electron normalization = ', normalization)
-    elif normalization == 'formula':
-        normalization = len(StrucTools(di['structure']).structure)/ CompTools(di['results']['formula']).n_atoms
-        print('formula normalization = ', normalization)
+    if not normalization:
+        normalization = 1
 
-    for ele in di['tdos']:
-        print(ele)
-        if ((ele == 'E') or (ele == 'up') or (ele == 'down')):
-            continue
+    if population_sources == "all":
+        tdos_keys = list(tdos.keys())
+        non_sources = ["E", "up", "down"]
+        population_sources = [k for k in tdos_keys if k not in non_sources]
 
-        population[ele] = [val/normalization for val in di['tdos'][ele]]
+    for src in population_sources:
+        orig = tdos[src]
+        to_plot = np.array(orig) / normalization
+        tdos[src] = to_plot
 
-
-    dos_lw = 1    
-
-    for element in what_to_plot:
-        if str(colors_and_labels[element]['color']) == 'black':
-            color = colors_and_labels[element]['color']
+    for el in population_sources:
+        if el in colors:
+            color = color_palette[colors[el]]
         else:
-            color = colors[colors_and_labels[element]['color']]
-        label = colors_and_labels[element]['label']
-                  
-        energies = di['tdos']['E']
-        populations = population[element]
+            color = color_palette[random.choice(list(color_palette.keys()))]
+
+        label = el
+        if special_labels:
+            if el in special_labels:
+                label = special_labels[el]
+
+        energies = tdos["E"]
+        populations = tdos[el]
+
         occ_energies = []
         occ_populations = []
         unocc_energies = []
         unocc_populations = []
-        
+
         for idx, E in enumerate(energies):
             if E == occupied_up_to:
                 occ_energies.append(energies[idx])
                 occ_populations.append(populations[idx])
                 unocc_energies.append(energies[idx])
                 unocc_populations.append(populations[idx])
-            elif E < occupied_up_to:
+            if E < occupied_up_to:
                 occ_energies.append(energies[idx])
                 occ_populations.append(populations[idx])
             elif E > occupied_up_to:
                 unocc_energies.append(energies[idx])
                 unocc_populations.append(populations[idx])
-                  
+
         # smearing with Gaussian filter
         if smearing:
             occ_populations = gaussian_filter1d(occ_populations, smearing)
             unocc_populations = gaussian_filter1d(unocc_populations, smearing)
-        ax = plt.plot(occ_populations, occ_energies, color=color, label=label, alpha=0.9, lw=dos_lw)
-        ax = plt.plot(unocc_populations, unocc_energies, color=color, label='__nolegend__', alpha=0.9, lw=dos_lw)                    
-        ax = plt.fill_betweenx(occ_energies, occ_populations, color=color, alpha=0.2, lw=0)                               
-         
-    ax = plt.axhline(y = 0, color = 'black', linestyle = '--')
 
+        ax = plt.plot(
+            occ_populations,
+            occ_energies,
+            color=color,
+            label=label,
+            alpha=params["line_alpha"],
+            lw=params["lw"],
+        )
+        ax = plt.plot(
+            unocc_populations,
+            unocc_energies,
+            color=color,
+            label="__nolegend__",
+            alpha=params["line_alpha"],
+            lw=params["lw"],
+        )
+        ax = plt.fill_betweenx(
+            occ_energies, occ_populations, color=color, alpha=params["fill_alpha"], lw=0
+        )
+
+    ax = plt.axhline(y=Efermi, color="black", linestyle="--")
 
     ax = plt.xticks(xticks[1])
     ax = plt.yticks(yticks[1])
     if not xticks[0]:
-        ax = plt.gca().xaxis.set_ticklabels([])      
+        ax = plt.gca().xaxis.set_ticklabels([])
     if not yticks[0]:
         ax = plt.gca().yaxis.set_ticklabels([])
     ax = plt.xlabel(xlabel)
     ax = plt.ylabel(ylabel)
     ax = plt.title(title)
     ax = plt.xlim(xlim)
-    ax = plt.ylim(ylim)   
-
+    ax = plt.ylim(ylim)
 
     if legend:
-        ax = plt.legend(loc = 'upper right')
+        if isinstance(legend, str):
+            ax = plt.legend(loc=legend)
+        else:
+            ax = plt.legend(loc="best")
+
     if show:
         plt.show()
-    if saveas:
-        plt.savefig(saveas)
-    
+
+    if savename:
+        plt.savefig(savename)
+
     return ax
 
 
 def main():
+    test_data_dir = "../data/test_data/vasp/AlN"
+    tdos = read_json(os.path.join(test_data_dir, "tdos.json"))
+    # return tdos
+    ax = ax_tdos(
+        tdos,
+        population_sources=["total", "Al"],
+        colors={"total": "black", "Al": "green"},
+        color_palette=get_colors("set2"),
+        params={"line_alpha": 0.9, "fill_alpha": 0.2, "lw": 1},
+        special_labels=None,
+        spins="summed",
+        normalization=1,
+        smearing=1,
+        Efermi=0.0,
+        xlim=(0, 10),
+        ylim=(-2, 2),
+        xticks=(True, [0, 5, 10]),
+        yticks=(True, [-2, -1, 0, 1, 2]),
+        xlabel="DOS",
+        ylabel=r"$E-E_F\/(eV)$",
+        legend=True,
+        title=None,
+        savename=os.path.join(test_data_dir, "tdos.png"),
+        show=True,
+    )
 
-    plot_tdos_switch = True
-    
-    d = make_smaller_results(xc = "metagga", remake=True)
-    dcp = d.copy()
-    
-    for key in dcp:
-        formula = dcp[key]["results"]["formula"]
-        eles = CompTools(formula).els
-        di = dcp[key]
-        
-        # please change the following line to the title you want
-        title = formula
-        normalization = 'formula'
-        
-        if normalization == 'formula':
-            xlabel = r'$DOS\/(per\/f.u.)$'
-        elif normalization == 'electron':
-            xlabel = r'$DOS\/(per\/e^{-})$'
-        
-        
-        if plot_tdos_switch:
-            plot_tdos(di = di,
-                formula = formula,
-                what_to_plot={ele for ele in eles},
-                colors_and_labels = {'Li' : {'color' : 'black', 'label' : 'Li'},
-                                    'P' : {'color' : 'gray', 'label' : 'P'}, 
-                                    'Mn' : {'color' : 'green', 'label' : "Mn"}, 
-                                    'S' : {'color' : 'pink', 'label' : 'S'}}, 
-                title = title,
-                saveas = formula + '.png',
-                normalization = normalization,
-                xlabel = xlabel)
-
-    return
+    return tdos
 
 
 if __name__ == "__main__":
-    main()
+    tdos = main()
