ee8bb8cd6ba84f38e2814ba73f26516b1a03e1c4
chen7916 <chen7916@umn.edu>
2023-06-21 14:12:02 -0500

    ax_tcohp

--- pydmclab/plotting/dos.py
+++ pydmclab/plotting/dos.py
@@ -233,3 +233,230 @@ def ax_tdos(
         plt.savefig(savename)
 
     return ax
+
+
+
+def ax_tcohp(
+    tcohp,
+    colors,
+    population_sources="all",
+    color_palette=get_colors("set2"),
+    params={"line_alpha": 0.9, "fill_alpha": 0.3, "lw": 1},
+    special_labels=None,
+    normalization=None,
+    flip = True,
+    bond_type = "cohp",
+    smearing=2.0,
+    Efermi=0.0,
+    xlim=(-6, 6),
+    ylim=(-2, 2),
+    xticks=(True, [-6, -4, -2, 0, 2, 4, 6]),
+    yticks=(True, [-2, -1, 0, 1, 2]),   
+    xlabel=r"$-COHP$",
+    ylabel=r"$E-E_F\/(eV)$",
+    legend=True,
+    title=None,
+    savename=False,
+    show=True,
+    ):
+
+    """
+    Args:
+        tcohp (dict)
+            result of pydmclab.hpc.analyze.tcohp()
+                list(d.keys()) = ['E'] + [el1-el2 (str) for all pairs of elements in structure that might be bonding]
+                    note: el1-el2 is sorted alphabetically
+                    e.g., the keys for Ca2Ru2O7 might be: ['Ca-Ca', 'Ca-Ru', 'Ca-O', 'Ru-Ru', 'O-Ru', 'O-O', 'E']
+                d['E'] = 1d array of energies corresponding with DOS
+                for each "bond type" (el1-el2), we have two keys: ['cohp', 'icohp']
+                    cohp --> absolute populations
+                    icohp --> integrated populations
+                    d[el1-el2]['cohp' or 'icohp'] will return a 1d array of populations summed over all interactions of el1-el2 in the structure, summed over all spins
+                so if I wanted to plot the total COHP for the interaction between Ru and O throughout the structure:
+                    energies = d['E']
+                    cohp = d['O-Ru']['cohp']
+                    plt.plot(cohp, energies)
+        
+        colors (dict)
+            {bond : color (str)}
+            
+        population_sources (str or list)
+            'all' : plot all bonds
+            ['S-S'] : plots just the S-S bonds
+            ['S-S', 'P-S'] : plots just S-S and P-S bonds
+
+
+        color_palette (dict)
+            {color (str) : rgb (tuple)}
+                usually loaded from pydmc.utils.plotting.get_colors()
+
+        params (dict)
+            {'fill_alpha' : transparency for occ populations,
+             'line_alpha' : transparency for COHP line,
+             'lw' : COHP linewidth}
+
+        special_labels (dict)
+            {bond : label (str)}
+
+        normalization (float)
+            divide COHP by this number
+                common normalizations:
+                    1 : no normalization (same as None)
+                    CompTools(formula).n_atoms (per formula unit)
+                    results['meta']['all_input_parameters']['NELECT'] (per electron)
+        
+        flip = (bool)
+            flip the COHP value to -COHP
+        
+        bond_type (str)
+            "cohp" or "icohp"
+        
+        smearing (float or False)
+            std. dev. for Gaussian smearing of COHP or False for no smearing
+
+        Efermi (float)
+            Fermi level (eV)
+
+        xlim (tuple)
+            (xmin (float), xmax (float))
+
+        ylim (tuple)
+            (ymin (float), ymax (float))
+
+        xticks (tuple)
+            (bool to show label or not, (xtick0, xtick1, ...))
+
+        xlabel (str)
+            x-axis label
+
+        ylabel (str)
+            y-axis label
+
+        legend (bool)
+            include legend
+
+        title (str)
+            title of plot
+
+        savename (str)
+            if False: don't save (just return ax)
+            if str, save fig object at this location
+
+        show (bool)
+            show plot
+
+    Returns:
+        matplotlib axis object
+    """
+    set_rc_params()
+    random.seed(42)
+
+    if savename:
+        fig = plt.figure(figsize=(5, 8))
+
+    Efermi = 0.0
+    occupied_up_to = Efermi
+    print("Fermi level = ", occupied_up_to)
+
+    if not normalization:
+        normalization = 1
+
+    if population_sources == "all":
+        tcohp_keys = list(tcohp.keys())
+        non_sources = ["E"]
+        population_sources = [k for k in tcohp_keys if k not in non_sources]
+
+    for src in population_sources:
+        orig = tcohp[src][bond_type]
+        if not flip:
+            to_plot = np.array(orig) / normalization
+        else:
+            to_plot = -np.array(orig) / normalization
+        tcohp[src] = to_plot
+
+    for bond in population_sources:
+        if bond in colors:
+            color = color_palette[colors[bond]]
+        else:
+            color = color_palette[random.choice(list(color_palette.keys()))]
+
+        label = bond
+        if special_labels:
+            if bond in special_labels:
+                label = special_labels[bond]
+
+        energies = tcohp['E']
+        populations = tcohp[bond]
+        
+        occ_energies = []
+        occ_populations = []
+        unocc_energies = []
+        unocc_populations = []
+        
+        for idx, E in enumerate(energies):
+            if E == occupied_up_to:
+                occ_energies.append(energies[idx])
+                occ_populations.append(populations[idx])
+                unocc_energies.append(energies[idx])
+                unocc_populations.append(populations[idx])
+            if E < occupied_up_to:
+                occ_energies.append(energies[idx])
+                occ_populations.append(populations[idx])
+            elif E > occupied_up_to:
+                unocc_energies.append(energies[idx])
+                unocc_populations.append(populations[idx])
+                
+
+        # smearing with Gaussian filter
+        if smearing:
+            occ_populations = gaussian_filter1d(occ_populations, smearing)
+            unocc_populations = gaussian_filter1d(unocc_populations, smearing)
+
+        ax = plt.plot(
+            occ_populations,
+            occ_energies,
+            color=color,
+            label=label,
+            alpha=params["line_alpha"],
+            lw=params["lw"],
+        )
+        ax = plt.plot(
+            unocc_populations,
+            unocc_energies,
+            color=color,
+            label="__nolegend__",
+            alpha=params["line_alpha"],
+            lw=params["lw"],
+        )
+        ax = plt.fill_betweenx(
+            occ_energies, occ_populations, color=color, alpha=params["fill_alpha"], lw=0
+        )
+        
+    ax = plt.axvline(x=0, color='black', linestyle='--')        
+    ax = plt.axhline(y=Efermi, color='black', linestyle='--')
+
+    ax = plt.xticks(xticks[1])
+    ax = plt.yticks(yticks[1])
+    if not xticks[0]:
+        ax = plt.gca().xaxis.set_ticklabels([])      
+    if not yticks[0]:
+        ax = plt.gca().yaxis.set_ticklabels([])
+    ax = plt.xlabel(xlabel)
+    ax = plt.ylabel(ylabel)
+    ax = plt.title(title)
+    ax = plt.xlim(xlim)
+    ax = plt.ylim(ylim)   
+
+    if legend:
+        if isinstance(legend, str):
+            ax = plt.legend(loc=legend)
+        else:
+            ax = plt.legend(loc="best")
+
+    if show:
+        plt.show()
+
+    if savename:
+        plt.savefig(savename)
+        
+    return ax
