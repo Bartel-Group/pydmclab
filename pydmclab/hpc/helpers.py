764534259351ac5fead6287d8cd3b09896503fd6
cbartel <cbartel@umn.edu>
2023-06-28 08:48:03 -0500

    mem --> mem_per_cpu

--- pydmclab/hpc/helpers.py
+++ pydmclab/hpc/helpers.py
@@ -183,7 +183,7 @@ def get_slurm_configs(
     else:
         mem_per_cpu = mem_per_core
 
-    slurm_configs["mem"] = str(int(mem_per_cpu * slurm_configs["ntasks"])) + "M"
+    slurm_configs["mem_per_cpu"] = str(int(mem_per_cpu)) + "M"
     return slurm_configs
 
 
