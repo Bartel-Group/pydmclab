d0268f90939653a7faad35b27ae35a54f1e48f18
Kyle-Nord <Kyle-Nord@users.noreply.github.com>
2024-11-07 15:29:37 -0600

    testing folder tree creation and copying launchers

--- hpc_workflows/slab_calcs/run.py
+++ hpc_workflows/slab_calcs/run.py
@@ -22,7 +22,6 @@ TREE = {
 # check if the project tree already exists
 # if it doesn't, assume this is the first time running the script and make the project tree/copy the launchers
 FIRST_RUN = False
-RECOPY_LAUNCHERS = False
 for k, v in TREE.items():
     for sub_dir in v:
         if not os.path.exists(os.path.join(CENTRAL_DIR, k, sub_dir)):
@@ -30,32 +29,6 @@ for k, v in TREE.items():
             break
     if FIRST_RUN:
         break
-
-if FIRST_RUN or RECOPY_LAUNCHERS:
-    # copy the template launcher to the correct scripts location
-    #  if you want to use a custom launcher, just set CUSTOM_TEMPLATE_LAUNCHER = True and put your launcher.py in the scripts dir
-    #  unless you're drasctially changing the template launcher, you shouldn't need to change this
-    CUSTOM_TEMPLATE_LAUNCHER = False
-    if not CUSTOM_TEMPLATE_LAUNCHER:
-        copyfile(f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_bulk-templates_launcher.py",
-                os.path.join(CENTRAL_DIR, "bulk-templates", "scripts", "launcher.py"))
-
-    # copy the slab launcher to the correct scripts location
-    #  if you want to use a custom launcher, just set CUSTOM_SLAB_LAUNCHER = True and put your launcher.py in the scripts dir
-    #  unless you're drasctially changing the slab launcher, you shouldn't need to change this
-    CUSTOM_SLAB_LAUNCHER = False
-    if not CUSTOM_SLAB_LAUNCHER:
-        copyfile(f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_slabs-launcher.py",
-                os.path.join(CENTRAL_DIR, "slabs", "scripts", "launcher.py"))
-
-    # copy the reference launcher to the correct scripts location
-    #  if you want to use a custom launcher, just set CUSTOM_REFERENCE_LAUNCHER = True and put your launcher.py in the scripts dir
-    #  unless you're drasctially changing the reference launcher, you shouldn't need to change this
-    CUSTOM_REFERENCE_LAUNCHER = False
-    if not CUSTOM_REFERENCE_LAUNCHER:
-        copyfile(f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_bulk-references-launcher.py",
-                os.path.join(CENTRAL_DIR, "bulk-references", "scripts", "launcher.py"))    
-
 
 # minimum info required to start the template calcs from a query
 # Note: This is not a complete list of possible inputs, just the minimum necessary info
@@ -89,12 +62,118 @@ def make_project_tree(parent_dir=CENTRAL_DIR, project_tree=TREE):
             os.makedirs(os.path.join(parent_dir, k, sub_dir), exist_ok=True)
     return None
 
-def copy_template_launchers(
 
+def copy_templates_launcher():
+    """
+    Copies the template launcher to the correct scripts location and replaces the minimum required inputs
+    """
+
+    # copy the template launcher
+    copyfile(
+        f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_bulk-templates_launcher.py",
+        os.path.join(CENTRAL_DIR, "bulk-templates", "scripts", "template-launcher.py"),
+    )
+
+    # read the template launcher
+    with open(
+        os.path.join(CENTRAL_DIR, "bulk-templates", "scripts", "template-launcher.py"),
+        "r",
+    ) as f:
+        content = f.read()
+
+    # replace the API_KEY and COMPOSITION
+    content = content.replace("{{API_KEY}}", API_KEY)
+    content = content.replace("{{COMPOSITION}}", COMPOSITION)
+
+    # write the updated template launcher
+    with open(
+        os.path.join(CENTRAL_DIR, "bulk-templates", "scripts", "template-launcher.py"),
+        "w",
+    ) as f:
+        f.write(content)
+
+    return None
+
+
+def copy_slabs_launcher():
+    """
+    Copies the slab launcher to the correct scripts location and replaces the minimum required inputs
+    """
+
+    # copy the slab launcher
+    copyfile(
+        f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_slabs-launcher.py",
+        os.path.join(CENTRAL_DIR, "slabs", "scripts", "slabs-launcher.py"),
+    )
+
+    # read the slab launcher
+    with open(
+        os.path.join(CENTRAL_DIR, "slabs", "scripts", "slabs-launcher.py"), "r"
+    ) as f:
+        content = f.read()
+
+    # replace the TEMPLATE_CIF_FILE, STRUC_ID, OVERRIDE_MAG, and MILLER_INDICES
+    content = content.replace("{{TEMPLATE_CIF_FILE}}", TEMPLATE_CIF_FILE)
+    content = content.replace("{{STRUC_ID}}", STRUC_ID)
+    content = content.replace("{{OVERRIDE_MAG}}", str(OVERRIDE_MAG))
+    content = content.replace("{{MILLER_INDICES}}", MILLER_INDICES)
+
+    # write the updated slab launcher
+    with open(
+        os.path.join(CENTRAL_DIR, "slabs", "scripts", "slabs-launcher.py"), "w"
+    ) as f:
+        f.write(content)
+
+    return None
+
+
+def copy_references_launcher():
+    """
+    Copies the reference launcher to the correct scripts location and replaces the minimum required inputs
+    """
+
+    # copy the reference launcher
+    copyfile(
+        f"{HOME_PATH}/bin/pydmclab/hpc_workflows/slab_calcs/demo_bulk-references-launcher.py",
+        os.path.join(
+            CENTRAL_DIR, "bulk-references", "scripts", "references-launcher.py"
+        ),
+    )
+
+    # read the reference launcher
+    with open(
+        os.path.join(
+            CENTRAL_DIR, "bulk-references", "scripts", "references-launcher.py"
+        ),
+        "r",
+    ) as f:
+        content = f.read()
+
+    # replace OVERRIDE_MAG
+    content = content.replace("{{OVERRIDE_MAG}}", str(OVERRIDE_MAG))
+
+    # write the updated reference launcher
+    with open(
+        os.path.join(
+            CENTRAL_DIR, "bulk-references", "scripts", "references-launcher.py"
+        ),
+        "w",
+    ) as f:
+        f.write(content)
+
+    return None
 
 
 def main():
+
+    if FIRST_RUN:
+        make_project_tree()
+        copy_templates_launcher()
+        copy_slabs_launcher()
+        copy_references_launcher()
+
     return None
+
 
 if __name__ == "__main__":
     main()
