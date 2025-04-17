a1b1fc0ae9affa507c7706e1d53848b7b0f2ec3f
lanne056 <lanne056@umn.edu>
2025-04-17 12:30:31 -0500

    chgnet relax workflow launcher

---
+++ hpc_workflows/chgnet_relax_calcs/relax_launcher.py
@@ -0,0 +1,159 @@
+import sys
+import os
+
+from pydmclab.utils.handy import read_json
+from pydmclab.hpc.helpers import check_strucs, check_launch_dirs
+
+HOME_PATH = os.environ["HOME"]
+RELAX_HELPERS_DIR = "%s/bin/pydmclab/hpc_workflows/chgnet_relax_calcs" % HOME_PATH
+
+if RELAX_HELPERS_DIR not in sys.path:
+    sys.path.append(RELAX_HELPERS_DIR)
+
+from relax_helpers import (
+    get_relax_configs,
+    get_launch_configs,
+    get_slurm_configs,
+    get_torch_configs,
+    setup_job,
+    make_relax_scripts,
+    make_submission_scripts,
+    submit_jobs,
+    collect_results,
+    check_collected_results,
+)
+
+# where is this file
+SCRIPTS_DIR = os.getcwd()
+
+# where are my calculations going to live (maybe on scratch)
+CALCS_DIR = SCRIPTS_DIR.replace("scripts", "calcs")
+
+# where is my data going to live
+DATA_DIR = SCRIPTS_DIR.replace("scripts", "data")
+
+# make our calcs dir and data dir
+for d in [CALCS_DIR, DATA_DIR]:
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+# set chgnet relaxation configs
+RELAX_CONFIGS = get_relax_configs(
+    model="0.3.0",
+    optimizer="FIRE",
+    stress_weight=1 / 160.21766208,
+    on_isolated_atoms="warn",
+    fmax=0.1,
+    steps=500,
+    relax_cell=True,
+    ase_filter="FrechetCellFilter",
+    params_asefilter=None,
+    interval=1,
+    verbose=False,
+)
+
+# set launch configs
+LAUNCH_CONFIGS = get_launch_configs(batch_size=100, save_interval=10)
+
+# set slurm submission script configs
+SLURM_CONFIGS = get_slurm_configs(
+    total_nodes=1,
+    cores_per_node=16,
+    walltime_in_hours=6,
+    mem_per_core_in_MB=1900,
+    partition="agsmall,msismall,msidmc",
+    error_file="log.e",
+    output_file="log.o",
+    account="cbartel",
+)
+
+# set torch configs
+TORCH_CONFIGS = get_torch_configs(
+    slurm_configs=SLURM_CONFIGS, num_intraop_threads=4, num_interop_threads=4
+)
+
+# collect all user configs
+USER_CONFIGS = {**RELAX_CONFIGS, **LAUNCH_CONFIGS, **SLURM_CONFIGS, **TORCH_CONFIGS}
+
+# location of relax_template.py
+RELAX_TEMPLATE = (
+    "%s/bin/pydmclab/hpc_workflows/chgnet_relax_calcs/relax_template.py" % HOME_PATH
+)
+
+
+def main():
+
+    # print strucs summary?
+    print_strucs_check = True
+
+    # re-run setup? print setup summary?
+    rerun_setup = False
+    print_launch_dirs_check = True
+
+    # remake relax scripts?
+    remake_relax_scripts = False
+
+    # remake submission scripts?
+    remake_subs = False
+
+    # submit jobs?
+    ready_to_launch = False
+
+    # remake results? print results summary?
+    remake_results = True
+    print_results_check = True
+
+    # ----------------------------
+
+    # retrieve structures
+    strucs = read_json(os.path.join(DATA_DIR, "strucs.json"))
+    if print_strucs_check:
+        check_strucs(strucs)
+
+    # run batching and directory setup
+    # if rerun_setup = True, will completely redo batching
+    #   and directory setup (all data in calcs will be lost)
+    batching = setup_job(
+        strucs=strucs,
+        user_configs=USER_CONFIGS,
+        calcs_dir=CALCS_DIR,
+        data_dir=DATA_DIR,
+        savename="batching.json",
+        rerun=rerun_setup,
+    )
+    if print_launch_dirs_check:
+        launch_dirs = [batching[batch_id]["launch_dir"] for batch_id in batching]
+        check_launch_dirs(launch_dirs)
+
+    # write relax scripts in each launch directory
+    make_relax_scripts(
+        batching=batching,
+        user_configs=USER_CONFIGS,
+        relax_template=RELAX_TEMPLATE,
+        remake=remake_relax_scripts,
+    )
+
+    # write submission scripts in each launch directory
+    make_submission_scripts(
+        batching=batching, user_configs=USER_CONFIGS, remake=remake_subs
+    )
+
+    # submit jobs
+    if ready_to_launch:
+        submit_jobs(batching=batching)
+
+    # collect results
+    results = collect_results(
+        batching=batching,
+        user_configs=USER_CONFIGS,
+        data_dir=DATA_DIR,
+        remake=remake_results,
+    )
+    if print_results_check:
+        check_collected_results(results=results, batching=batching)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
