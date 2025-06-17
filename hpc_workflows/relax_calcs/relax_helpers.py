1609cb23634e9642d19aaf6fc0f17fff48e3175e
lanne056 <lanne056@umn.edu>
2025-06-17 15:36:59 -0500

    fix sub file python call

--- hpc_workflows/relax_calcs/relax_helpers.py
+++ hpc_workflows/relax_calcs/relax_helpers.py
@@ -496,7 +496,7 @@ def make_submission_scripts(
             f.write(f"#SBATCH --job-name={job_name}\n")
             f.write(f"#SBATCH --partition={user_configs['partition']}\n")
             f.write("\n")
-            f.write("python chgnet_relax.py\n")
+            f.write(f"python {architecture.lower()}-{model}-prediction.py\n")
 
         print(f"\nCreated new submission script for {launch_dir}")
 
