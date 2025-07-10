7abbc8ec60dcdf92bce029c2b67b0fadc4915cda
lipsc070 <lipsc070@umn.edu>
2025-07-10 16:59:57 -0500

    getting rid of add_args as a fairchem arg

--- hpc_workflows/chgnet_md_calcs/md_helpers.py
+++ hpc_workflows/chgnet_md_calcs/md_helpers.py
@@ -377,7 +377,13 @@ def make_md_scripts(
     """
 
     architecture, model = get_model(user_configs)
-    md_configs_plural = ["ensembles", "thermostats", "temperatures", "steps"]
+    md_configs_plural = [
+        "ensembles",
+        "thermostats",
+        "temperatures",
+        "steps",
+        "add_args",
+    ]
     md_configs_singular = ["ensemble", "thermostat", "temperature"]
 
     for launch_dir, settings in launch_dirs.items():
